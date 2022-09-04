d43c98679accd4db195b666aefa4ab636855820b
Yevhen <73534149+Krenichniy@users.noreply.github.com>
2022-09-04 23:18:11 +0300

    added  header menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
+import { Menu } from "modules/Menu";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Menu/>
     </div>
   );
 };
 // test
+  // style={{
+  //       height: '100vh',
+  //       display: 'flex',
+  //       justifyContent: 'center',
+  //       alignItems: 'center',
+  //       fontSize: 40,
+  //       color: '#010101'
+  //     }}
